38bb4b15b5de9346cbb67aaee6ee9940e6f84259
Tom <Tom@TOM-PC>
2025-04-14 19:16:18 +0200

    Projektdateien hinzufügen.

---
+++ Sotierprogramm 1.1/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace Sotierprogramm_1._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.btnStart = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 40);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Tai Le", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(3, 3);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(197, 34);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Bilder Sortieren";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Sotierprogramm_1._1.Properties.Resources.free_minus_icon_3108_thumb;
+            this.pictureBox2.Location = new System.Drawing.Point(733, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(26, 34);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            this.pictureBox2.MouseEnter += new System.EventHandler(this.pictureBox2_MouseEnter);
+            this.pictureBox2.MouseLeave += new System.EventHandler(this.pictureBox2_MouseLeave);
+            this.pictureBox2.MouseHover += new System.EventHandler(this.pictureBox2_MouseHover);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Sotierprogramm_1._1.Properties.Resources.x_icon_150997;
+            this.pictureBox1.Location = new System.Drawing.Point(765, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(32, 34);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseEnter += new System.EventHandler(this.pictureBox1_MouseEnter);
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            this.pictureBox1.MouseHover += new System.EventHandler(this.pictureBox1_MouseHover);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(199, 173);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "QuellBilder";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(300, 170);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(186, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(498, 170);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(25, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(199, 243);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ZielBilder";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(300, 240);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(186, 23);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(498, 243);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(24, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(350, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Anzahl im Ordner: 0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(350, 266);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Anzahl im Ordner: 0";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(382, 421);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "0 / 0";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(376, 399);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Loading";
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(203, 341);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(399, 42);
+            this.btnStart.TabIndex = 11;
+            this.btnStart.Text = "Start | Sort";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Sotierprogramm 1.1/Form1.cs
@@ -0,0 +1,372 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sotierprogramm_1._1
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        public Point mouseLocation;
+        private bool isDragging = false;
+        private Point lastCursor;
+        private Point lastForm;
+
+
+        public string QuellBilder;
+        public string ZielBilder;
+
+        public int anzahlimQuell;
+        public int anzahlimZiel;
+
+        public int a = 0;
+        public int b = 0;
+        public int c = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label6.Visible = false;
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            if (e.Button == MouseButtons.Left)
+            {
+                isDragging = true;
+                lastCursor = Cursor.Position;
+                lastForm = this.Location;
+            }
+        }
+
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            if (isDragging)
+            {
+                Point delta = Point.Subtract(Cursor.Position, (Size)lastCursor);
+                this.Location = Point.Add(lastForm, (Size)delta);
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            base.OnMouseUp(e);
+            isDragging = false;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            pictureBox1.Padding = new Padding(0);
+            pictureBox1.Margin = new Padding(0);
+            pictureBox2.Padding = new Padding(0);
+            pictureBox2.Margin = new Padding(0);
+
+        }
+
+        private void pictureBox1_MouseHover(object sender, EventArgs e)
+        {
+            pictureBox1.BackColor = Color.Red;
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            pictureBox1.BackColor = Color.Transparent;
+
+        }
+
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+
+            pictureBox1.BackColor = Color.Red;
+        }
+
+        private void pictureBox2_MouseEnter(object sender, EventArgs e)
+        {
+            pictureBox2.BackColor = Color.LightBlue;
+
+
+        }
+
+        private void pictureBox2_MouseHover(object sender, EventArgs e)
+        {
+            pictureBox2.BackColor = Color.LightBlue;
+
+        }
+
+        private void pictureBox2_MouseLeave(object sender, EventArgs e)
+        {
+            pictureBox2.BackColor = Color.Transparent;
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            folderDlg.ShowNewFolderButton = true;
+            DialogResult result = folderDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBox1.Text = folderDlg.SelectedPath;
+                Environment.SpecialFolder root = folderDlg.RootFolder;
+            }
+            if (Directory.Exists(textBox1.Text))
+            {
+                int totalFileCount = Directory.GetFiles(textBox1.Text, "*.*", SearchOption.AllDirectories).Length;
+
+
+                label3.Text = "Anzahl im Ordner: " + totalFileCount.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Der angegebene Ordner existiert nicht.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            folderDlg.ShowNewFolderButton = true;
+            DialogResult result = folderDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBox2.Text = folderDlg.SelectedPath;
+                Environment.SpecialFolder root = folderDlg.RootFolder;
+            }
+
+            if (Directory.Exists(textBox2.Text))
+            {
+                int totalFileCount = Directory.GetFiles(textBox2.Text, "*.*", SearchOption.AllDirectories).Length;
+
+
+                label4.Text = "Anzahl im Ordner: " + totalFileCount.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Der angegebene Ordner existiert nicht.");
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (textBox1.Text + textBox2.Text != "")
+            {
+                btnStart.Enabled = true;
+            }
+            else
+            {
+                btnStart.Enabled = false;
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            timer3.Start();
+            label6.Visible = true;
+            if (string.IsNullOrWhiteSpace(textBox1.Text) || string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                MessageBox.Show("Bitte gebe deine Quell- und Zielordner an!");
+                return;
+            }
+
+            if (!Directory.Exists(textBox1.Text) || !Directory.Exists(textBox2.Text))
+            {
+                MessageBox.Show("Ein oder beide angegebene Ordner existieren nicht.");
+                return;
+            }
+
+            QuellBilder = textBox1.Text;
+            ZielBilder = textBox2.Text;
+
+            label6.Text = "Loading...";
+            label6.ForeColor = Color.Red;
+
+            await ProcessFilesAsync(); 
+
+            label6.Text = "Finish";
+            label6.ForeColor = Color.Green;
+        }
+
+        private async Task ProcessFilesAsync()
+        {
+            try
+            {
+                DirectoryInfo sourceDirectory = new DirectoryInfo(QuellBilder);
+                DirectoryInfo destinationDirectory = new DirectoryInfo(ZielBilder);
+
+                UpdateFileCountLabels(sourceDirectory, destinationDirectory);
+
+                var files = sourceDirectory.GetFiles("*", SearchOption.AllDirectories);
+
+                foreach (var file in files)
+                {
+                    await Task.Run(() => ProcessFile(file, destinationDirectory));
+                    UpdateProgress();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("FEHLER! " + ex.Message);
+            }
+        }
+
+        private void UpdateFileCountLabels(DirectoryInfo sourceDir, DirectoryInfo destinationDir)
+        {
+            int sourceFileCount = Directory.GetFiles(sourceDir.FullName, "*.*", SearchOption.AllDirectories).Length;
+            int destinationFileCount = Directory.GetFiles(destinationDir.FullName, "*.*", SearchOption.AllDirectories).Length;
+
+            anzahlimQuell = sourceFileCount;
+            anzahlimZiel = destinationFileCount;
+
+            
+            Invoke((MethodInvoker)delegate
+            {
+                label3.Text = $"Anzahl im Quellordner: {anzahlimQuell}";
+                label4.Text = $"Anzahl im Zielordner: {anzahlimZiel}";
+            });
+        }
+
+        private void ProcessFile(FileInfo file, DirectoryInfo destinationDir)
+        {
+            a++;
+            DateTime fileDate = file.LastWriteTime;
+            string year = fileDate.ToString("yyyy");
+            string month = fileDate.ToString("MM");
+            string day = fileDate.ToString("dd");
+
+            string relativePath = GetRelativePath(file.FullName, QuellBilder);
+
+            string newPath = Path.Combine(destinationDir.FullName, year, GetMonthName(month), day);
+
+            Directory.CreateDirectory(newPath);
+
+            if (file.Name == "Thumbs.db") return;
+
+            try
+            {
+                string destinationFilePath = Path.Combine(newPath, file.Name);
+                File.Copy(file.FullName, destinationFilePath, true);
+                b++;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Fehler beim Kopieren der Datei {file.Name}: {ex.Message}");
+            }
+        }
+
+        private string GetRelativePath(string filePath, string rootPath)
+        {
+            Uri fileUri = new Uri(filePath);
+            Uri rootUri = new Uri(rootPath);
+            Uri relativeUri = rootUri.MakeRelativeUri(fileUri);
+            return Uri.UnescapeDataString(relativeUri.ToString());
+        }
+
+        private string GetMonthName(string month)
+        {
+            string monthName;
+            switch (month)
+            {
+                case "01":
+                    monthName = "01. - Januar";
+                    break;
+                case "02":
+                    monthName = "02. - Februar";
+                    break;
+                case "03":
+                    monthName = "03. - März";
+                    break;
+                case "04":
+                    monthName = "04. - April";
+                    break;
+                case "05":
+                    monthName = "05. - Mai";
+                    break;
+                case "06":
+                    monthName = "06. - Juni";
+                    break;
+                case "07":
+                    monthName = "07. - Juli";
+                    break;
+                case "08":
+                    monthName = "08. - August";
+                    break;
+                case "09":
+                    monthName = "09. - September";
+                    break;
+                case "10":
+                    monthName = "10. - Oktober";
+                    break;
+                case "11":
+                    monthName = "11. - November";
+                    break;
+                case "12":
+                    monthName = "12. - Dezember";
+                    break;
+                default:
+                    monthName = "99. - unbekannt";
+                    break;
+            }
+            return monthName;
+        }
+
+        private void UpdateProgress()
+        {
+            Invoke((MethodInvoker)delegate
+            {
+                label5.Text = b.ToString() + " / " + anzahlimQuell;
+
+                label4.Text = "Anzahl im Ordner: " + b.ToString();
+            });
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
